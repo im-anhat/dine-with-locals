bb7203c47a876753bd98a0a92c2db576ab24b9d5
Dan Nguyen <danuyennguyen2412005@gmail.com>
2025-04-15 03:16:07 -0400

    Structure the files for sign up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { AuthProvider } from './contexts/ExampleAuthContext';
 import Home from './pages/ExampleHome';
 import './styles/main.css';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/authentication/Login';
+import Signup from './pages/authentication/Signup';
 
+//The type React.FC is a type definition for type checking for functional components
+//and ensures that children are implicitly typed
 const App: React.FC = () => {
   return (
-    <AuthProvider>
+    <div>
       <Home />
-    </AuthProvider>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 };
 
