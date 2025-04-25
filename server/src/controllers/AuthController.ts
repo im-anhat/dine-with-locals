c25dd0d0588948a544d57d704dd250469502ab0c
Dan Nguyen <danuyennguyen2412005@gmail.com>
2025-04-25 12:52:20 -0400

    added comment to controllers/AuthController.ts

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -1,37 +1,38 @@
 import UserModel from '../models/User.js';
 import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
-import { SALT } from '../seeds/constants.js';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { UserLogin, User } from '../../../shared/types/User.js';
 
-dotenv.config(); //Load environment variables from .env file
+//Load environment variables from .env file
+dotenv.config();
 
-//Create token
 /**
- *A client sends username/password combination to the server
- *The server validates the authentication
- *If authentication is successful, the server creates a JWT token else establishes an error response
-  On successful authentication, the client gets JWT token in the response body
-  Client stores that token in local storage or session storage.
-  From next time, the client for making any request supplies the JWT token in request headers like this. Authorization: Bearer <jwt_token>
-  Server upon receiving the JWT validates it and sends the successful response else error.
- * 
+ * Creates a JWT token for the user
+ * @param _id - The user's ID
+ * @returns The JWT token as a string
  */
-const createToken = (_id: any) => {
-  return jwt.sign({ _id }, process.env.SECRET!, { expiresIn: '3d' }); //If you want to access the key in .env, you need to use dontenv
+const createToken = (_id: any): string => {
+  //jwt.sign(payload, secretOrPrivateKey, [options, callback])
+  return jwt.sign({ _id }, process.env.SECRET!, { expiresIn: '3d' });
   // Use `!` to tell TypeScript you're sure it won't be undefined
 };
 
-// login a user
+/**
+ * This function handles user login.
+ * It receives the user login data from the request body,
+ * attempts to log in the user using the UserModel,
+ * and if successful, creates a JWT token for the user.
+ * If there's an error, it sends a 400 response with the error message.
+ * @param req - The request object containing the user login data
+ * @param res - The response object used to send the response
+ * @returns A JSON response containing the JWT token or an error message
+ */
 const loginUser = async (req: Request, res: Response) => {
   const userLogin: UserLogin = req.body;
 
   try {
     const user = await UserModel.login(userLogin);
-
-    // create a token
     const token = createToken(user._id);
     res.status(200).json({ token });
   } catch (error: any) {
@@ -39,6 +40,16 @@ const loginUser = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * This function handles user signup.
+ * It receives the user signup data from the request body,
+ * attempts to create a new user using the UserModel,
+ * and if successful, creates a JWT token for the user.
+ * If there's an error, it sends a 400 response with the error message.
+ * @param req - The request object containing the user signup data
+ * @param res - The response object used to send the response
+ * @returns A JSON response containing the JWT token or an error message
+ */
 const signupUser = async (req: Request, res: Response) => {
   const { userSignup } = req.body;
 
