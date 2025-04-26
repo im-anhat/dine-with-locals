df5bd1f09188107eca04ecbd33ad9e8ec8822578
Dan Nguyen <danuyennguyen2412005@gmail.com>
2025-04-25 21:50:07 -0400

    Finish sign up feature backend

--- client/src/hooks/auth/useSignup.tsx
+++ client/src/hooks/auth/useSignup.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { User } from '../../../../shared/types/User';
+import BASE_URL from '../../../../shared/constants/constants';
 
 export const useSignUp = () => {
+  console.log(`${BASE_URL}api/auth/signup`);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const signup = async (user: User) => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await axios.post('', user); //API endpoint here
+      const response = await axios.post(`${BASE_URL}api/auth/signup`, user); //API endpoint here
       console.log('Signup successful:', response.data);
     } catch (err: any) {
       console.error('Signup error:', err);
