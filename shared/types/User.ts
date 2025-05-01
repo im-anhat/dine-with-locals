1c78fb7a58ab79c91dbffab879408339913bb829
Dan Nguyen <danuyennguyen2412005@gmail.com>
2025-04-30 21:14:08 -0400

    removed redundant interface declaration in shared/types/User.ts

--- shared/types/User.ts
+++ shared/types/User.ts
@@ -14,20 +14,5 @@ export interface User {
   bio: string;
 }
 
-export interface UserLogin {
-  userName: string;
-  password: string;
-}
-
-export interface AuthenticatedUser {
-  userName: string;
-  firstName: string;
-  lastName: string;
-  phone: string;
-  avatar: string;
-  socialLink: string;
-  role: 'Host' | 'Guest' | 'Both';
-  hobbies: string[];
-  ethnicity: 'Asian' | 'Black' | 'Hispanic' | 'White' | 'Other';
-  bio: string;
-}
+type AuthenticatedUser = Omit<User, 'password'>;
+type UserLogin = Pick<User, 'userName' | 'password'>;
