e7a9861fb07bf37a9cd0eb44a5669a2305cbfc70
Tam Nguyen <tamngminh3@gmail.com>
2025-05-14 17:33:58 +0700

    upload listing to mongodb + cloudinary

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,25 @@
 import express from 'express';
 import connectDB from './config/mongo.js';
-import userRoutes from './routes/ExampleUserRoutes.js';
+import cors from 'cors';
+// import userRoutes from './routes/ExampleUserRoutes.js';
 import errorHandler from './middlewares/ExampleErrorHandler.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+import listingRoutes from './routes/listingRoutes.js';
 
 const app = express();
 
 connectDB();
 
-// app.use(express.json());
-// app.use('/api/users', userRoutes);
-// app.use(errorHandler);
+// Middleware
+app.use(express.json());
+app.use(cors())
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Routes
+// app.use('/api/users', userRoutes);
+app.use('/api/upload', uploadRoutes);
+app.use('/api/listings', listingRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
