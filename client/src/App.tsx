f052905f41e63c474e2e4f35e30bea25a7403ff2
Nhat Le <anhat.ptnk2023@gmail.com>
2025-05-17 12:31:51 -0500

    modify Places

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import Home from './pages/ExampleHome';
 import './styles/main.css';
 import { Routes, Route } from 'react-router-dom';
-import Login from './pages/auth/Login';
-import SignUpPage from './pages/auth/SignUpPage';
-import ExampleHome from './pages/ExampleHome';
+
+import Places from './pages/Places';
 
 //The type React.FC is a type definition for type checking for functional components
 //and ensures that children are implicitly typed
 const App: React.FC = () => {
   return (
     <div>
-      <Home />
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/" element={<ExampleHome />} />
+        <Route path="/" element={<Places />} />
       </Routes>
     </div>
   );
--- client/src/pages/Places.tsx
+++ client/src/pages/Places.tsx
@@ -1,6 +1,17 @@
 import React, { useState } from 'react';
 
 const Places = () => {
+  const [selectedPlace, setSelectedPlace] =
+    useState<google.maps.places.PlaceResult | null>(null);
+  const [location, setLocation] = useState({
+    lat: 40.7128,
+    lng: -74.006,
+  });
+  const isHost = currentUser.role === 'Host';
+  const handlePlaceSelect = (place: google.maps.places.PlaceResult) => {
+    setSelectedPlace(place);
+  };
+
   return <div>Places</div>;
 };
 
