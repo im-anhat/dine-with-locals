b0ed6a3922fe3579e00c261aa572bba33ac704e2
Nhat Le <anhat.ptnk2023@gmail.com>
2025-05-17 21:49:51 -0500

    map init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,19 @@ import './styles/main.css';
 import { Routes, Route } from 'react-router-dom';
 
 import Places from './pages/Places';
+import { UserProvider } from './contexts/UserContext';
 
 //The type React.FC is a type definition for type checking for functional components
 //and ensures that children are implicitly typed
 const App: React.FC = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Places />} />
-      </Routes>
-    </div>
+    <UserProvider>
+      <div>
+        <Routes>
+          <Route path="/" element={<Places />} />
+        </Routes>
+      </div>
+    </UserProvider>
   );
 };
 
--- client/src/components/places/MapLoader.tsx
+++ client/src/components/places/MapLoader.tsx
@@ -24,7 +24,7 @@ const MapLoader: React.FC<MapLoaderProps> = ({ children }) => {
       };
 
       const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&callback=initMap`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&libraries=places&callback=initMap`;
       script.async = true;
       script.defer = true;
 
