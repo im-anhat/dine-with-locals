a1767b6e426d78a39684a2d2b351a06fd2a4e813
Tam Nguyen <tamngminh3@gmail.com>
2025-05-27 22:43:06 +0700

    fix ui bug at /dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -50,7 +50,7 @@ const App: React.FC = () => {
         <Route
           path="/dashboard"
           element={
-            isAuthenticated ? <DashboardPage /> : <Navigate to="/" replace />
+            isAuthenticated ? <DashboardPage /> : <div></div>
           }
         />
       </Routes>
