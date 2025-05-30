40595028b59b1e70306dc0596cdb4154c0ad2cc4
Tam Nguyen <tamngminh3@gmail.com>
2025-05-31 00:35:28 +0700

    modified middleware to attach usr to req

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import connectDB from './config/mongo.js';
 import userRoutes from './routes/UserRoutes.js';
 import authRoutes from './routes/AuthRoutes.js';
+import chatRoutes from './routes/ChatRoutes.js';
 import locationRoutes from './routes/LocationRoutes.js';
 
 // Import all models first to ensure they're registered with mongoose
@@ -15,6 +16,8 @@ import './models/Location.js';
 import './models/Match.js';
 import './models/Request.js';
 import './models/Review.js';
+import './models/Chat.js';
+import './models/Message.js';
 
 // Import routes
 import reviewRoutes from './routes/ReviewRoutes.js';
@@ -26,14 +29,14 @@ connectDB();
 
 //Parse user request -> Json format
 app.use(express.json());
-//Only receive request from some specific routes.
 app.use(cors());
+
+// Routes
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/location', locationRoutes);
 app.use('/api/reviews', reviewRoutes);
-
-// Routes
+app.use('/api/chat', chatRoutes);
 
 // Start server
 const PORT = process.env.PORT || 3000;
