3258681f7410eeaaea0b40f95116e948b1744bd5
Dan Nguyen <danuyennguyen2412005@gmail.com>
2025-07-08 08:08:54 -0400

    changes before merge

--- client/src/pages/pending/ListingDashboard.tsx
+++ client/src/pages/pending/ListingDashboard.tsx
@@ -29,7 +29,7 @@ function ListingDashboard({ ...props }: PendingCardProps) {
         </span>
       </div>
 
-      <ListingDashboard />
+      {/* <ListingDashboard /> */}
       <GuestDetailsCard />
       <PaymentStatusCard />
       <GuestReviewCard />
